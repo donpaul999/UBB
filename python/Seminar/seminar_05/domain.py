80cea2a3edb6fc01ff0e683e6355910eb536b4eb
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2019-10-29 10:52:06 +0200

    Update Seminar

--- python/Seminar/seminar_05/domain.py
+++ python/Seminar/seminar_05/domain.py
@@ -10,20 +10,33 @@ class Location:
     '''
     x,y,z - integers
     '''
+    def __init__(self,x,y,z):
+        self._x = x
+        self._y = y
+        self._z = z
     pass
 
 def test_location():
     alderaan = Location(1, 2, 3) # x,y,z
 
+    assert alderaan.X == 1 and alderaan.Y  == 2 and alderaan.Z == 3
+    alderaan.X = 10
+    assert alderaan.X == 10
+    alderaan.Y += 5
+    assert alderaan.Y == 7
+
+    '''
     alderaan.get_x()
 
     # allows me to validate star's position
     alderaan.set_x() # you run code, so you can validate
     alderaan.set_x(alderaan.get_x() + 50) # ok, but ugly
     #Python properties to the resq!
-    alderaan.x += 50 # cide runsm so you can validate, and looks reasonable
+    alderaan.X += 50 # cide runsm so you can validate, and looks reasonable
 
     #alderaan._x = 5 # no code runs, so no validations possiblw
+    '''
+
 
 test_location()
 
