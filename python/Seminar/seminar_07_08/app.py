31f3fa037606a16657f0439991b6c73fa5f59b52
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2019-11-19 11:53:19 +0200

    update

--- python/Seminar/seminar_07_08/app.py
+++ python/Seminar/seminar_07_08/app.py
@@ -27,8 +27,25 @@ Assignment 01 - 05
 '''
 
 clientRepo = ClientRepository()
+clientRepo.store((cl1 := Client(1,"Pop Maria", 20)))
+clientRepo.store((cl2 := Client(2,"Vlad Maria", 25)))
 #service needs a repo that stores entities
 clientService = clientService(clientRepo)
+carRepo = Repository()
+carRepo.store( (car1 := Car(1, "CJ01ERT", "Dacia", "Lodgy", "red")) )
+carRepo.store( (car2 := Car(1, "CJ01XVT", "Vovlo", "XC60", "red")) )
 
-ui = UI(clientService)
+rentalRepo = Repository()
+rentalRepo.store(Rental(1,date(2010,11,20), date(2010,11,30), cl1, car1))
+rentalRepo.store(Rental(2,date(2016,12,25), date(2017,11,20), cl1, car1))
+
+#Services
+carService = CarService(carRepo)
+clientService = ClientService(clientRepo)
+rentalService = RentalService(rentalRepo)
+
+#UI and start program
+ui = UI(carService, clientService, rentalService)
 ui.start()
+
+
