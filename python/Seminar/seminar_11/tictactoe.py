3c33db682d36a79a646d85f65a8b395f6f24f1a7
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2019-12-10 10:40:20 +0200

    Update tictactoe.py

--- python/Seminar/seminar_11/tictactoe.py
+++ python/Seminar/seminar_11/tictactoe.py
@@ -30,6 +30,19 @@ class Board:
         # X - 1
         # O - -1
 
+    def move(self, x, y, symbol):
+        #x, y in [0,1,2], symbol in [X,O]
+        d = {'O': -1 , 'X': 1}
+        if x not in [0,1,2] or y not in [0,1,2]:
+            raise ValueError("Move not inside the board!")
+        if self._data[3 * x + y] != 0:
+            raise ValueError("Square is already taken!")
+        if symbol not in ['X', 'O']:
+            raise ValueError("Bad Symbol!")
+
+        self._data[3 * x + y] = d[symbol]
+
+
     def __str__(self):
         t = Texttable()
         d = {-1: 'O', 0: ' ', 1: 'X'}
@@ -42,4 +55,7 @@ class Board:
 
 
 b = Board()
+b.move(1,1,'X')
+b.move(0,0, 'O')
+b.move(2,2, 'X')
 print(b)
