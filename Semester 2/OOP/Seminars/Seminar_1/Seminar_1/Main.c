10d040bb5bf60000f51ece01771113f608398c72
Paul Colța <43109542+donpaul999@users.noreply.github.com>
2020-03-03 13:51:10 +0200

    update

--- Semester 2/OOP/Seminars/Seminar_1/Seminar_1/Main.c
+++ Semester 2/OOP/Seminars/Seminar_1/Seminar_1/Main.c
@@ -1,6 +1,7 @@
 #include "Planet.h"
 #include "Repository.h"
 #include <crtdbg.h>
+#include "Service.h"
 int main() {
 	return 0;
 
---
+++ Semester 2/OOP/Seminars/Seminar_1/Seminar_1/Service.c
@@ -0,0 +1,22 @@
+#include "Service.h"
+#include <stdlib.h>
+
+Service* createService(Repository* repo)
+{
+	Service* service = (Service*)malloc(sizeof(Service));
+	service->repo = repo;
+	return service;
+}
+
+void destroyService(Service* service)
+{
+	destroyRepository(service->repo);
+	free(service);
+	service = 0;
+}
+
+void addPlanetService(Service* service, char* name, char* type, double distance)
+{
+	Planet p = createPlanet(name, type, distance);
+	addPlanet(service->repo, p);
+}
